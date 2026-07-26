dda7a52e62871271653e5aae3b7e3579d0bf4638
Areeba Naeem <areebanaeem069@gmail.com>
2026-07-26 21:51:12 +0500

    Create weather_activity.py

---
+++ 10ConditionalProblems/weather_activity.py
@@ -0,0 +1,11 @@
+input = input("What is your weather today ?").lower()
+
+if input=="sunny":
+    print ("Go for a walk ")
+elif input=="rainy":
+    print("Read book")
+elif input=="snowy":
+    print("build a snow man ")
+
+
+    
